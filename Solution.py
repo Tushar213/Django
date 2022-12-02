ce66df87a2a1f2c18da9da2d85164f0bdd65fafa
Tushar Trivedi <35912864+Tushar-98@users.noreply.github.com>
2022-12-03 02:37:24 +0530

    finishing touches

--- Solution.py
+++ Solution.py
@@ -7,8 +7,7 @@ def calculate_savings(events):
     host = events["host"]
     user = events["user"] 
     password = events["password"]
-    date = "-".join(events["date"].split("/")[::-1])
-    # print (date)
+    date = "-".join(events["date"].split("/")[::-1]) # convert date from dd/mm/yyyy to yyyy-mm-dd
     try:
         conn = psycopg2.connect(
         database=database, user=user, password=password, host=host, port= port
@@ -19,26 +18,28 @@ def calculate_savings(events):
 
         #query to get the age and avg savings
         sql = """select t.age,CAST (AVG(t.total) AS INTEGER) from
-        (select t.total, date_part('year',age(DOB)) :: int as age from 
-        (select DATE(c.date_of_birth) AS DOB, t.customer_id, t.total from
-        (select t.customer_id, SUM(t.total) as total from
-        (select t.customer_id, CASE when t.txn_type = 'credit' then t.txn_amount else (t.txn_amount*-1) END total from
-        (select customer_id,txn_type,txn_amount from Transactions where DATE(transaction_date) = %s) as t) as t group by t.customer_id)
-        as t FULL JOIN Customer as c on t.customer_id = c.customer_id ) as t) as t Group by t.age"""
+          (select t.total, date_part('year',age(DOB)) :: int as age from 
+            (select DATE(c.date_of_birth) AS DOB, t.customer_id, t.total from
+              (select t.customer_id, SUM(t.total) as total from
+                (select t.customer_id, CASE when t.txn_type = 'credit' then t.txn_amount else (t.txn_amount*-1) END total from
+                  (select customer_id,txn_type,txn_amount from Transactions where DATE(transaction_date) = %s) as t)
+                    as t group by t.customer_id) as t FULL JOIN Customer as c on t.customer_id = c.customer_id ) as t) 
+                      as t Group by t.age"""
 
-        # sql = """select * from Transactions"""
+        # execute the query 
         cursor.execute(sql,(date,))
+        
+        # fetch the output and store it in val
         val = cursor.fetchall()
         data = {}
         for x in val:
             data[x[0]] = x[1]
     except Exception as e:
-        logging.error(traceback.format_exc())
         return {"statusCode": 400, "message": 'An exception occurred: {}'.format(e)}
-    # print (val)
+    
     return {"statusCode": 200, "data": data}
 
-
+#Main Function
 if __name__ == "__main__":
     events = {}
     events["database"] = "postgres"
