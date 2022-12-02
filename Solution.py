9a8f6432b298585429b012be5ec474ba3c81558e
Tushar Trivedi <35912864+Tushar-98@users.noreply.github.com>
2022-12-03 02:25:45 +0530

    code

--- Solution.py
+++ Solution.py
@@ -1,25 +1,52 @@
 import psycopg2
 
+def calculate_savings(events):
+    # establishing the connection
+    database = events["database"]
+    port = events["port"]
+    host = events["host"]
+    user = events["user"] 
+    password = events["password"]
+    date = "-".join(events["date"].split("/")[::-1])
+    # print (date)
+    try:
+        conn = psycopg2.connect(
+        database=database, user=user, password=password, host=host, port= port
+        )
 
-# establishing the connection
-conn = psycopg2.connect(
-   database="postgres", user='postgres', password='password', host='127.0.0.1', port= '5433'
-)
+        #CREATE cursor object
+        cursor = conn.cursor()
 
-#CREATE cursor object
-cursor = conn.cursor()
+        #query to get the age and avg savings
+        sql = """select t.age,CAST (AVG(t.total) AS INTEGER) from
+        (select t.total, date_part('year',age(DOB)) :: int as age from 
+        (select DATE(c.date_of_birth) AS DOB, t.customer_id, t.total from
+        (select t.customer_id, SUM(t.total) as total from
+        (select t.customer_id, CASE when t.txn_type = 'credit' then t.txn_amount else (t.txn_amount*-1) END total from
+        (select customer_id,txn_type,txn_amount from Transactions where DATE(transaction_date) = %s) as t) as t group by t.customer_id)
+        as t FULL JOIN Customer as c on t.customer_id = c.customer_id ) as t) as t Group by t.age"""
 
-#query to get the age and avg savings
-sql = """select t.age,CAST (AVG(t.total) AS INTEGER) from
-(select t.total, date_part('year',age(DOB)) :: int as age from 
-(select DATE(c.date_of_birth) AS DOB, t.customer_id, t.total from
-(select t.customer_id, SUM(t.total) as total from
-(select t.customer_id, CASE when t.txn_type = 'credit' then t.txn_amount else (t.txn_amount*-1) END total from
-(select customer_id,txn_type,txn_amount from Transactions where DATE(transaction_date) = %s) as t) as t group by t.customer_id)
-as t FULL JOIN Customer as c on t.customer_id = c.customer_id ) as t) as t Group by t.age"""
+        # sql = """select * from Transactions"""
+        cursor.execute(sql,(date,))
+        val = cursor.fetchall()
+        data = {}
+        for x in val:
+            data[x[0]] = x[1]
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        return {"statusCode": 400, "message": 'An exception occurred: {}'.format(e)}
+    # print (val)
+    return {"statusCode": 200, "data": data}
 
-# sql = """select * from Transactions"""
-cursor.execute(sql,('2022-12-02',))
 
-val = cursor.fetchall()
-print (val)
+if __name__ == "__main__":
+    events = {}
+    events["database"] = "postgres"
+    events["port"] = "5433"
+    events["host"] = "127.0.0.1"
+    events["user"] = "postgres"
+    events["password"] = "password"
+    events["date"] = "02/12/2022"
+    print (calculate_savings(events))
+    
+
